3a4f932a645bde5c23ebaeabadd7f586dcd7e9b5
rob-murray <robmurray17@gmail.com>
2012-10-05 13:05:43 +0100

    Added method to display annotation for point

--- OSMKDemoApp/OSMKDemoApp/MapViewController.m
+++ OSMKDemoApp/OSMKDemoApp/MapViewController.m
@@ -32,18 +32,40 @@
     
     self.mapView.delegate = self;
     
-    [self setMapDefaultLocation];
+    [self setMapDefaultLocation];//set region to gb
+    
+    [self addAnnotationForCoord:(CLLocationCoordinate2D){52.205298,0.118146}];//add coord for point
+
 }
 
+/*
+ * Simple method to add annotation for a CLLocationCoordinate2D
+ */
+- (void)addAnnotationForCoord:(CLLocationCoordinate2D)coord
+{
+    
+    MKPointAnnotation *annotation = [MKPointAnnotation new];
+    [annotation setCoordinate:coord];
+    
+    annotation.title = @"Here my coordinate";
+    annotation.subtitle = [NSString stringWithFormat:@"%0.4f,%0.4f",annotation.coordinate.latitude, annotation.coordinate.longitude];
+    [self.mapView addAnnotation:annotation];
+    
+}
+
+/*
+ * Set default location
+ */
 - (void)setMapDefaultLocation
 {
-    // go to the UK
+    // go to the GB
     MKCoordinateRegion newRegion;
     newRegion.center.latitude = 54;
     newRegion.center.longitude = -1.5;
     newRegion.span.latitudeDelta = 8;
     newRegion.span.longitudeDelta = 7;
     [self.mapView setRegion:newRegion animated:NO];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -53,5 +75,24 @@
     
     self.mapView = nil;
 }
+
+
+#pragma mark Map View Delegate methods
+
+-(MKAnnotationView*)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
+{
+    // Use the default user location view.
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+    {
+        return nil;
+    }
+    
+    MKPinAnnotationView *view = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+    view.animatesDrop = YES;
+    view.canShowCallout = YES;
+    view.draggable = NO;
+    return view;
+}
+
 
 @end
